d4705f76edeb8c792064d155a3d28f5086bef6e0
8dcc <8dcc.git@gmail.com>
2022-10-04 13:11:47 +0200

    Add compensate.out to binary-table.sh

--- misc/binary-table.sh
+++ misc/binary-table.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 bin="conversor.out"
+compensate="compensate.out"
+buf="buffer.txt"
+
 rows=$1
 
+echo "" > $buf
+
 for (( i=0; i<=rows; i++ )); do
-# for i in {0..$rows}; do
     # Decimal to binary
-    ./$bin d b $i
+    ./$bin d b $i >> $buf
 done
+
+./$compensate $rows < $buf
+rm $buf
